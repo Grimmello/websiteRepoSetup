d5d16147eb455752cff8639da247126a1d5a2516
Clifford Grimmell and Xi Xia <xi@>
2017-02-09 17:04:26 -0800

    adds index and bootstrap in setup files

--- websiteRepoSetup.sh
+++ websiteRepoSetup.sh
@@ -4,7 +4,8 @@ mkdir ~/Desktop/$1
 mkdir ~/Desktop/$1/css
 mkdir ~/Desktop/$1/img
 touch ~/Desktop/$1/css/styles.css
-touch ~/Desktop/$1/index.html
+cp ~/Desktop/websiteRepoSetup/index.html ~/Desktop/$1/index.html
+cp ~/Desktop/websiteRepoSetup/css/bootstrap.css ~/Desktop/$1/css/bootstrap.css
 cd ~/Desktop/$1
 git init
 git pair $2 $3
