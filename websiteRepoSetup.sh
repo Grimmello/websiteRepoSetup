c042322802f47e5b69ec110426ada98abf1a638f
Clifford Grimmell <agrimmell2@gmail.com>
2018-03-15 23:26:26 -0700

    add single user use case to project

--- websiteRepoSetup.sh
+++ websiteRepoSetup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#usage ./websiteRepoSetup.sh dirName pairInitial1 pairInitial2
+#usage ./websiteRepoSetup.sh dirName
 mkdir ~/Desktop/$1
 mkdir ~/Desktop/$1/css
 mkdir ~/Desktop/$1/img
@@ -9,7 +9,6 @@ cp -R ~/Desktop/websiteRepoSetup/css ~/Desktop/$1/
 cp -R ~/Desktop/websiteRepoSetup/js ~/Desktop/$1/
 cd ~/Desktop/$1
 git init
-git pair $2 $3
 git add .
-git-pair-commit -m "create initial css folder, img folder, and index.html"
+git commit -m "create initial css folder, img folder, and index.html"
 atom .
